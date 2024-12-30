ea7f2a9a83c87d3620ca0d1c6955ef95b17712c7
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-12-30 11:56:31 +0700

    Create the User Entity

---
+++ src/main/java/com/dev/blog/domain/entities/User.java
@@ -0,0 +1,56 @@
+package com.dev.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    /*
+        * We need to implement equals and hashCode explicitly because
+        * Lombok's implementation can cause issues with JPA entities
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, name, createdAt);
+    }
+
+    // When a new user is created, JPA will automatically call our onCreate() method to set the creation timestamp.
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
